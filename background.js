c0bf77525acff337604a1338594193938aedf4de
George Cave <gcbristol@gmail.com>
2018-03-13 22:48:34 +0100

    Added contentEditable functionality

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
-// Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-    // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' red!');
-    chrome.tabs.executeScript(null, {file: "jquery-3.3.1.js"});
-    chrome.tabs.executeScript(null, {file: "content_script.js"});
+    chrome.tabs.executeScript(null, { file: "jquery-3.3.1.min.js" }, function() {
+        chrome.tabs.executeScript(null, { file: "content_script.js" });
+    });
   });
--- content_script.js
+++ content_script.js
@@ -1,3 +1,22 @@
 $(function(){
- //   $("P").hover
+
+    var old_bg_color;
+    var hover_styles = {
+        backgroundColor : "rgba(92,199,21,0.4)",
+        cursor: "default",
+    };
+
+    $("H1, H2, H3, H4, H5, H6, P").hover(
+        function(){
+            old_bg_color = $(this).css('background-color');
+            $(this).css(hover_styles);
+        },
+        function(){
+            $(this).css('background-color', old_bg_color);
+        }
+    ).mouseup(function(){
+        $(this).attr("contentEditable",true).focus();
+    }).blur(function(){
+        $(this).attr("contentEditable",false);        
+    });
 });
